980736026558e13d6a4da988500861a8e51aeb9a
Joel Watson <Joel.watson92@gmail.com>
2021-03-10 13:25:12 -0800

    finished section seven (also has some of c# course)

--- ASPNET-Angular-From-Scratch/DatingApp/client/src/app/app.module.ts
+++ ASPNET-Angular-From-Scratch/DatingApp/client/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorsComponent
+    TestErrorsComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
---
+++ Complete-CSharp-Masterclass/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.WriteLine("Hello World!");
+
+            //byte number = 1;
+            //int integer = 20;
+            //char character = 'A';
+            //bool isWorking = true;
+            //float floater = 20.01f;
+            //double doubler = 30.02;
+            //decimal decimaler = 40.03m;
+            //string firstName = "Joel";
+            // or you can just use var
+            var number = 1;
+            var integer = 20;
+            var character = 'A';
+            var isWorking = true;
+            var floater = 20.01f;
+            var doubler = 30.02;
+            var decimaler = 40.03m;
+            var firstName = "Joel";
+
+            Console.WriteLine(isWorking);
+            Console.WriteLine(character);
+            Console.WriteLine(number);
+            Console.WriteLine(integer);
+            Console.WriteLine(floater);
+            Console.WriteLine(doubler);
+            Console.WriteLine(decimaler);
+            Console.WriteLine(firstName);
+
+
+        }
+    }
+}
