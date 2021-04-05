b9ca31aa3a7896e158f90991c1e4a3181ae729a9
Joel Watson <Joel.watson92@gmail.com>
2021-04-05 09:56:39 -0700

    finished interview course

--- Coding-Interview-Bootcamp/events/index.js
+++ Coding-Interview-Bootcamp/events/index.js
@@ -4,16 +4,32 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+
+  constructor() {
+    this.events = {};
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.events[eventName] === undefined) this.events[eventName] = [];
+    this.events[eventName].push(callback);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for(let i=0; i<this.events[eventName].length; i++) {
+        this.events[eventName][i]();
+      }
+    }    
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
--- Coding-Interview-Bootcamp/sorting/index.js
+++ Coding-Interview-Bootcamp/sorting/index.js
@@ -2,19 +2,57 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i=0; i<arr.length; i++) {
+    for (let k=0; k<arr.length-(i+1); k++) {
+      if(arr[k] > arr[k+1]) {
+        let lesser = arr[k+1];
+        arr[k+1] = arr[k];
+        arr[k] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
-
+  for (let i=0; i<arr.length; i++) {
+    let index = i;
+    for (let j=i; j<arr.length; j++) {
+      if (arr[index] > arr[j]) index = j;
+    }
+    let lesser = arr[index];
+    arr[index] = arr[i];
+    arr[i] = lesser;
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
+  if (arr.length == 1) return arr;
 
+  let mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-
+  const merged = [];
+  while(left.length && right.length) {
+    if (left[0] < right[0]) {
+      merged.push(left.shift())
+    } else {
+      merged.push(right.shift());
+    }
+  }
+  if (left.length) {
+    merged.push(...left);
+  }
+  if (right.length) {
+    merged.push(...right);
+  }
+  return merged
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
