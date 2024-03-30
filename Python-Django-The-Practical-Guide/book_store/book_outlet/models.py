aef530d7f64093054faf4ad9bbada24fbb5e86a5
Joel Watson <Joel.Watson92@gmail.com>
2024-03-29 20:56:00 -0700

    sync

---
+++ Python-Django-The-Practical-Guide/book_store/book_outlet/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class Book(models.Model):
+  title = models.CharField(max_length=50)
+  rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+  author = models.CharField(null=True, max_length=100)
+  is_best_seller = models.BooleanField(default=False)
+
+  def __str__(self):
+    return f"{self.title} ({self.rating})"
